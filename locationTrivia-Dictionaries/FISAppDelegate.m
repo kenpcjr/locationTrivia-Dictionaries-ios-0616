1ae6dce34bad4d38228ef85fa0c880c89e7e2831
kenpcjr <kennethcooke@me.com>
2016-06-02 14:53:53 -0400

    Done.

--- locationTrivia-Dictionaries/FISAppDelegate.m
+++ locationTrivia-Dictionaries/FISAppDelegate.m
@@ -10,10 +10,54 @@
     return YES;
 }
 
-/** 
- 
- *  Write your method implementations here.
- 
- */
+-(NSString *)stringByTruncatingNameOfLocation:(NSDictionary *)location toLength:(NSUInteger)length{
+    NSString *shortenedString = @"";
+    NSString *stringToShorten = location[@"name"];
+    NSUInteger stringLength = [stringToShorten length];
+    if (length <= stringLength) {
+        shortenedString = [stringToShorten substringToIndex:length];
+        return shortenedString;
+    }
+    return stringToShorten;
+}
+
+-(NSDictionary *)dictionaryForLocationWithName:(NSString *)name latitude:(CGFloat)latitude longitude:(CGFloat)longitude{
+    NSMutableDictionary *locationDict = [@{@"name" : name,
+                                          @"latitude" : @(latitude),
+                                           @"longitude" : @(longitude)}mutableCopy];
+    return locationDict;
+}
+
+-(NSArray *)namesOfLocations:(NSArray *)locations{
+    NSMutableArray *keyNames = [[NSMutableArray alloc]init];
+    for (NSDictionary *dict in locations) {
+        [keyNames addObject:dict[@"name"]];
+    }
+    return keyNames;
+}
+
+-(BOOL)dictionaryIsValidLocation:(NSDictionary *)dictionary{
+    NSMutableArray *dictKeys = [[dictionary allKeys]mutableCopy];
+    NSMutableArray *expectedKeys = [@[@"name", @"latitude", @"longitude"]mutableCopy];
+    NSSortDescriptor *alphabetizeKeys = [NSSortDescriptor sortDescriptorWithKey:nil ascending:YES];
+    [dictKeys sortUsingDescriptors:@[alphabetizeKeys]];
+    [expectedKeys sortUsingDescriptors:@[alphabetizeKeys]];
+    
+    BOOL containsCorrectKeys = NO;
+    if ([dictKeys isEqualToArray:expectedKeys]) {
+        containsCorrectKeys = YES;
+    }
+    
+    return containsCorrectKeys;
+}
+
+-(NSDictionary *)locationNamed:(NSString *)name inLocations:(NSArray *)locations{
+    for (NSDictionary *dict in locations) {
+        if ([name isEqualToString:dict[@"name"]]) {
+            return dict;
+        }
+    }
+    return nil;
+}
 
 @end
